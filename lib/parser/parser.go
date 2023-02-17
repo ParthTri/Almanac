cc9007b620523cb6a526b53cf8b9c38fbc9b2b55
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-02-17 16:48:48 +1300

    Creating findDate function that passes tests

--- lib/parser/parser.go
+++ lib/parser/parser.go
@@ -19,3 +19,24 @@ type Events struct {
 
 type Almanac []Events;
 
+// Check 10 bytes of data to match for date syntax YYYY-MM-DD
+//	Hyphens = 4, 7
+//	Rest numbers
+func findDate(data []byte) (string, error) {
+	var date string
+
+	if data[4] == 45 && data[7] == 45 {
+		for index, val := range data {
+			if (val > 48 || val < 57) || index == 4 || index == 7{
+				date += string(val)
+			} else {
+				return "", errors.New("Next set")
+			}
+		}
+	} else {
+		return "", errors.New("Next set")
+	}
+
+	return date, nil
+}
+
